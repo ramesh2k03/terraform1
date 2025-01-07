ae76c4fc65e649be7780d7fd271daf0dc359ca96
kommagoni Ramesh <kommagoni.ramesh@hcl.com>
2025-01-07 15:23:17 +0530

    functions

--- ec-2/ec2.tf
+++ ec-2/ec2.tf
@@ -1,7 +1,8 @@
 # resource <resource-type> <resource-name>
 resource "aws_instance" "db" {
 
-    ami = "ami-090252cbe067a9e58"
+    ami = "ami-09c813fb71547fc4f"
+
     vpc_security_group_ids = [aws_security_group.allow_ssh.id]
     instance_type = "t3.micro"
 
